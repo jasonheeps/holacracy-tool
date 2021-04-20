4949c3dacc09cc94f2e8f48610b60e24cac466b7
Philipp Schaefer <pschaefer@posteo.net>
2021-04-20 20:36:54 +0200

    remove policy route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'overview', to: 'pages#overview'
   resources :circles, only: [:index, :show]
   resources :roles, only: [:index, :show]
-  resources :policies, only: [:index]
   resources :employees, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
