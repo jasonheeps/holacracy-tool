a75652b55ed97fe791255834e8e4fab9c09b32fe
Philipp Schaefer <pschaefer@posteo.net>
2021-04-24 17:19:43 +0200

    define enum in shifts as hash

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,7 +1,15 @@
 class Shift < ApplicationRecord
   belongs_to :role_filling
 
-  enum weekday: %i[monday tuesday wednesday thursday friday saturday sunday]
+  enum weekday: {
+    monday: 0,
+    tuesday: 1,
+    wednesday: 2,
+    thursday: 3,
+    friday: 4,
+    saturday: 5,
+    sunday: 6
+  }
 
   def employee
     id = RoleFilling.find_by_id(role_filling_id).employee_id
