14ce0ee34f1dcc870cbd787a41fa4248c32b7ae8
Philipp Schaefer <pschaefer@posteo.net>
2021-06-07 10:32:48 +0200

    'my roles' on dashboard (not done yet)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,8 @@ class PagesController < ApplicationController
 
   def user_dashboard
     set_shifts
+    @employee = current_user.employee
+    set_roles_data
   end
 
   def user_profile
@@ -21,57 +23,19 @@ class PagesController < ApplicationController
 
   private
 
-  # TODO: Remove html methods after testing (moved to circle.rb)
-  # def init_circles_html
-  #   circles = Circle.all
-  #   # TODO: find most outer circle by more general attribute like 'level'
-  #   gcc = circles.find_by(acronym: 'GCC')
-  #   circles_data_hash = gcc.circle_to_hash
-  #   "<div class='circle-0'>\n
-  #     <a href='/circles/#{gcc.id}'>\n
-  #       <div class='circle-title-container'>\n
-  #         <h2 class='circle-title's>GCC</h2>\n
-  #       </div>\n
-  #     </a>
-  #     #{create_sub_circles_html(circles_data_hash)}
-  #     #{create_roles_html(gcc.roles)}
-  #   </div>"
-  # end
-
-  # def create_sub_circles_html(circles_hash)
-  #   html = ""
-  #   sub_circles_hash = circles_hash[:sub_circles]
-  #   sub_circles_hash.each do |sc_hash|
-  #     sc = sc_hash[:circle]
-  #     html += "
-  #       <div class='subcircle'>\n
-  #         <a href='/circles/#{sc.id}'>\n
-  #           <div class='circle-title-container'>\n
-  #             <h2 class='circle-title'>#{sc.title}</h2>\n
-  #           </div>\n
-  #         </a>
-  #         #{create_sub_circles_html(sc_hash)}\n
-  #         #{create_roles_html(sc.roles)}
-  #       </div>\n"
-  #   end
-  #   return html
-  # end
-
-  # def create_roles_html(roles)
-  #   html = ""
-  #   # TODO: migrate r.acronym and use it here instead of title if possible
-  #   roles.each do |r|
-  #     html += "
-  #       <div class='role'>\n
-  #         <a href='/roles/#{r.id}'>\n
-  #           <div class='role-title-container'>\n
-  #             <p class='role-title'>#{r.title}</p>\n
-  #           </div>\n
-  #         </a>\n
-  #       </div>\n"
-  #   end
-  #   return html
-  # end
+  def set_roles_data
+    roles = @employee.roles_sorted
+    @roles_data = roles.map do |r|
+      {
+        role: r,
+        acronym: r.acronym,
+        title: r.title,
+        primary_circle: r.primary_circle,
+        secondary_circle: r.secondary_circle,
+        status: r.status_string(@employee)
+      }
+    end
+  end
 
   def set_shifts
     shifts = current_user.employee.shifts
